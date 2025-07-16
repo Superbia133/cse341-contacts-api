6fbc9eb1daf2e5b01a81169b9074e2b8fbc19095
Superbia133 <egriffeth@byupathway.edu>
2025-07-15 21:12:07 -0700

    Fix export for setupSwagger function

--- backend/swagger.js
+++ backend/swagger.js
@@ -1,22 +1,9 @@
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger-output.json');
 
-const doc = {
-  info: {
-    title: 'CSE341 Contacts API',
-    description: 'API for managing contacts for the CSE341 course',
-  },
-  host: 'localhost:8080',
-  schemes: ['http'],
-  basePath: '/api',
-  tags: [
-    {
-      name: 'Contacts',
-      description: 'Contact management endpoints',
-    },
-  ],
-};
+// Define and export the setupSwagger function
+function setupSwagger(app) {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+}
 
-const outputFile = './swagger-output.json';
-const endpointsFiles = ['./server.js']; // can include additional files
-
-swaggerAutogen(outputFile, endpointsFiles, doc);
+module.exports = setupSwagger;
